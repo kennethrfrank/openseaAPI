ee8c0417a7d1738f6d677ee9d0aa1d6eee109232
kennethrfrank <kennethrfrankjr@gmail.com>
2022-10-25 06:38:08 -0500

    yyy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.state = {
+        monsters: [],
+        nfts: [],
+        nclyneNfts: [],
+    };
+  }
+
+  componentDidMount(){
+   /* fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then((users) => this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          },
+
+    ));*/
+
+    fetch('https://api.opensea.io/api/v1/assets?owner=0x5116936f1B52F8545995f2549101Ab1A41920309&order_direction=desc&limit=200&include_orders=false').then(response => response.json()).then((assets) => this.setState(
+          () => {
+
+            
+            return { nfts: assets.assets };
+          },
+          () => {
+            console.log(this.state);
+          },
+    ));
+  }
+
+  render(){
+
+    
+    return (
+      <div className="App">
+       {
+        this.state.nfts.map((nft) => {
+          return<img key={nft.id} width="200px" alt="" src={nft.image_url}></img>;
+        })
+       }
+      </div>
+    ); 
+  }
+
 }
 
 export default App;
